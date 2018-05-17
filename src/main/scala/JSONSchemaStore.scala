3d15bb830b54f39bd288e3635ec446eb5387097e
Amartya Vadlamani <amartya.vadlamani@gmail.com>
2018-05-17 21:17:51 +0100

    created implemented JSONSchemaStore.set

--- src/main/scala/JSONSchemaStore.scala
+++ src/main/scala/JSONSchemaStore.scala
@@ -2,16 +2,32 @@ import com.github.fge.jackson.JsonLoader
 import com.github.fge.jsonschema.main.{JsonSchema, JsonSchemaFactory}
 
 class JSONSchemaStore(keyValueStore: KeyValueStore){
-  def get(id: String) : Option[JsonSchema] =
+
+  def set(id: String, schema: String) = {
+    JsonSchemaFactory.byDefault().getJsonSchema(JsonLoader.fromString(schema))
+    keyValueStore.set(id, schema)
+  }
+
+
+  def getSchema(id: String) : Option[JsonSchema] =
     try {
-      return keyValueStore
-        .get(id)
+      getSchemaText(id)
         .map(JsonLoader.fromString)
         .map(JsonSchemaFactory.byDefault().getJsonSchema)
     } catch {
       case e: Exception => return None
     }
 
+  def getSchemaText(id: String) =
+    try {
+      keyValueStore
+        .get(id)
+        .map(JsonLoader.fromString)
+        .map(JsonSchemaFactory.byDefault().getJsonSchema)
+      keyValueStore.get(id)
+    } catch {
+      case e: Exception => None
+    }
 
 
 }
